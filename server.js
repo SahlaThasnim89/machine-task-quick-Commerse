e41974812b19402ff7b9f1c3b000c8450e4ba36b
SAHLA THASNIM <152372740+SahlaThasnim89@users.noreply.github.com>
2025-02-24 19:17:43 +0530

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,8 @@ const next = require("next");
 const { Server } = require("socket.io");
 
 const dev = process.env.NODE_ENV !== "production";
-const hostname = "localhost";
+//const hostname = "localhost";
+const hostname = "0.0.0.0";
 const port = 5500;
 // when using middleware `hostname` and `port` must be provided below
 const app = next({ dev, hostname, port });
