44998860ed77928d6390b1d9dab5790bd101c4c9
SahlaThasnim89 <sahlathasnim2002@gmail.com>
2025-02-26 13:55:57 +0530

    my order ui change

--- app/my-orders/page.jsx
+++ app/my-orders/page.jsx
@@ -44,44 +44,75 @@ const MyOrders = () => {
             <div className="flex flex-col justify-between px-6 md:px-16 lg:px-32 py-6 min-h-screen">
                 <div className="space-y-5">
                     <h2 className="text-lg font-medium mt-6">My Orders</h2>
-                    {loading ? <Loading /> : (<div className="max-w-5xl border-t border-gray-300 text-sm">
-                        {orders.length&&orders.map((order, index) => (
-                            <div key={index} className="flex flex-col md:flex-row gap-5 justify-between p-5 border-b border-gray-300">
-                                <div className="flex-1 flex gap-5 max-w-80">
-                                    <Image
-                                        className="max-w-16 max-h-16 object-cover"
-                                        src={assets.box_icon}
-                                        alt="box_icon"
-                                    />
-                                    <p className="flex flex-col gap-3">
-                                        <span className="font-medium text-base">
-                                            {order.products.map((item) => item.id + ` x ${item.quantity}`).join(", ")}
-                                        </span>
-                                        <span>Items : {order.products.length}</span>
-                                    </p>
-                                </div>
-                                <div>
-                                    <p>
-                                        <span className="font-medium">{order.address.fullName}</span>
-                                        <br />
-                                        <span >{order.address.area}</span>
-                                        <br />
-                                        <span>{`${order.address.city}, ${order.address.state}`}</span>
-                                        <br />
-                                        <span>{order.address.phoneNumber}</span>
-                                    </p>
-                                </div>
-                                <p className="font-medium my-auto">{currency}{order.orderAmount}</p>
-                                <div>
-                                    <p className="flex flex-col">
-                                        <span>Method : COD</span>
-                                        <span>Date : {new Date(order.date).toLocaleDateString()}</span>
-                                        <span>Payment : Pending</span>
-                                    </p>
-                                </div>
-                            </div>
-                        ))}
-                    </div>)}
+                    {loading ? (
+        <Loading />
+      ) : orders.length === 0 ? (
+        <div className="flex flex-col items-center justify-center h-full">
+          <Image
+            src={assets.box_icon} 
+            alt="No Orders"
+            width={150}
+            height={150}
+          />
+          <p className="text-gray-500 text-lg mt-4">No orders found</p>
+        </div>
+      ) : (
+        <div className="md:p-10 p-4 space-y-5">
+          <h2 className="text-lg font-medium">Orders</h2>
+          <div className="max-w-4xl rounded-md">
+            {orders.map((order, index) => (
+              <div
+                key={index}
+                className="flex flex-col md:flex-row gap-5 justify-between p-5 border-t border-gray-300"
+              >
+                <div className="flex-1 flex gap-5 max-w-80">
+                  <Image
+                    className="max-w-16 max-h-16 object-cover"
+                    src={assets.box_icon}
+                    alt="box_icon"
+                  />
+                  <p className="flex flex-col gap-3">
+                    <span className="font-medium">
+                      {order.items
+                        .map(
+                          (item) => item.product.name + ` x ${item.quantity}`
+                        )
+                        .join(", ")}
+                    </span>
+                    <span>Items : {order.items.length}</span>
+                  </p>
+                </div>
+                <div>
+                  <p>
+                    <span className="font-medium">
+                      {order.address.fullName}
+                    </span>
+                    <br />
+                    <span>{order.address.area}</span>
+                    <br />
+                    <span>{`${order.address.city}, ${order.address.state}`}</span>
+                    <br />
+                    <span>{order.address.phoneNumber}</span>
+                  </p>
+                </div>
+                <p className="font-medium my-auto">
+                  {currency}
+                  {order.amount}
+                </p>
+                <div>
+                  <p className="flex flex-col">
+                    <span>Method : COD</span>
+                    <span>
+                      Date : {new Date(order.date).toLocaleDateString()}
+                    </span>
+                    <span>Payment : Pending</span>
+                  </p>
+                </div>
+              </div>
+            ))}
+          </div>
+        </div>
+      )}
                 </div>
             </div>
             <Footer />
